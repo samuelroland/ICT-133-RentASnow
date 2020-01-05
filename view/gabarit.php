fa76db0dfed9bec92c6b70ea217f9820aba2e7d1
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-01-05 09:23:44 +0100

    wip

--- view/gabarit.php
+++ view/gabarit.php
@@ -160,9 +160,10 @@
     <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
 
 
-    <script src="assets/carousel/jquery.carouFredSel-6.2.0-packed.js" type="text/javascript"></script><script type="text/javascript">$('#list_photos').carouFredSel({ responsive: true, width: '100%', scroll: 2, items: {width: 320,visible: {min: 2, max: 6}} });</script><script src="view/content/scripts/camera/scripts/camera.min.js" type="text/javascript"></script>
+    <script src="assets/carousel/jquery.carouFredSel-6.2.0-packed.js" type="text/javascript"></script>
+    <script type="text/javascript">$('#list_photos').carouFredSel({ responsive: true, width: '100%', scroll: 2, items: {width: 320,visible: {min: 2, max: 6}} });</script>
+    <script src="assets/camera/scripts/camera.min.js" type="text/javascript"></script>
     <script src="assets/easing/jquery.easing.1.3.js" type="text/javascript"></script>
-
     <script type="text/javascript">function startCamera() {$('#camera_wrap').camera({ fx: 'scrollLeft', time: 2000, loader: 'none', playPause: false, navigation: true, height: '35%', pagination: true });}$(function(){startCamera()});</script>
 
 </body>
