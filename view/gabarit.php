034d93761ee62a47904648652b6059b581810968
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-01-05 09:36:22 +0100

    Getting there...

--- view/gabarit.php
+++ view/gabarit.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title><?=$title;?></title>
+    <title><?= $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
@@ -10,13 +10,13 @@
     <link href="node_modules/bootstrap/dist/css/bootstrap-reboot.css" rel="stylesheet">
 
     <!-- Icons -->
-    <link href="assets/icons/general/stylesheets/general_foundicons.css" media="screen" rel="stylesheet" type="text/css" />
-    <link href="assets/icons/social/stylesheets/social_foundicons.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="assets/icons/general/stylesheets/general_foundicons.css" media="screen" rel="stylesheet" type="text/css"/>
+    <link href="assets/icons/social/stylesheets/social_foundicons.css" media="screen" rel="stylesheet" type="text/css"/>
 
     <link rel="stylesheet" href="assets/fontawesome/css/font-awesome.min.css">
 
-    <link href="assets/carousel/style.css" rel="stylesheet" type="text/css" />
-    <link href="assets/camera/css/camera.css" rel="stylesheet" type="text/css" />
+    <link href="assets/carousel/style.css" rel="stylesheet" type="text/css"/>
+    <link href="assets/camera/css/camera.css" rel="stylesheet" type="text/css"/>
 
     <link href="http://fonts.googleapis.com/css?family=Syncopate" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css">
@@ -25,7 +25,11 @@
     <link href="http://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet" type="text/css">
 
-    <link href="css/subtlepatterns/custom.css" rel="stylesheet" type="text/css" />
+    <link href="css/subtlepatterns/custom.css" rel="stylesheet" type="text/css"/>
+
+    <script src="node_modules/jquery/dist/jquery.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+
 </head>
 <body id="pageBody">
 
@@ -37,7 +41,7 @@
         <div class="row-fluid">
             <div class="span12">
                 <div id="divLogo" class="pull-left">
-                    <a href="index.php" id="divSiteTitle">Rent A Snow</a><br />
+                    <a href="index.php" id="divSiteTitle">Rent A Snow</a><br/>
                     <a href="index.php" id="divTagLine">La glisse à moindre coût</a>
                 </div>
                 <div id="divMenuRight" class="pull-right">
@@ -62,7 +66,7 @@
             <div class="divPanel notop page-content">
                 <div class="row-fluid">
                     <div class="span12" id="divMain">
-                        <?=$content; ?>
+                        <?= $content; ?>
                     </div>
                 </div>
                 <div id="footerInnerSeparator"></div>
@@ -73,98 +77,69 @@
 
         <div id="divFooter" class="footerArea">
             <div class="divPanel">
-                <div class="row-fluid">
-                    <div class="span3" id="footerArea1">
+                <div class="row">
+                    <div class="col-4" id="footerArea1">
                         <h3>Notre magasin</h3>
                         <p>Nous sommes une équipe de jeunes snowboardeurs qui souhaitons faire découvrir cette discipline à tous les publics.</p>
                         <p>
-                            <a href="#" title="Terms of Use">Terms of Use</a><br />
-                            <a href="#" title="Privacy Policy">Privacy Policy</a><br />
-                            <a href="#" title="FAQ">FAQ</a><br />
+                            <a href="#" title="Terms of Use">Terms of Use</a><br/>
+                            <a href="#" title="Privacy Policy">Privacy Policy</a><br/>
+                            <a href="#" title="FAQ">FAQ</a><br/>
                             <a href="#" title="Sitemap">Sitemap</a>
                         </p>
                     </div>
 
-                    <div class="span3" id="footerArea2">
-                        <h3>Dernière nouveautés</h3>
-                        <p>
-                            <a href="#" title="">Le Burton B221 est à disposition</a><br />
-                            <span style="text-transform:none;">15/05/2019</span>
-                        </p>
-                        <p>
-                            <a href="#" title="">Les horaires ont été étendus</a><br />
-                            <span style="text-transform:none;">02/01/2019</span>
-                        </p>
-                        <p>
-                            <a href="#" title="">Paul champion suisse !</a><br />
-                            <span style="text-transform:none;">19/03/2019</span>
-                        </p>
-                        <p>
-                            <a href="#" title="">VIEW ALL POSTS</a>
-                        </p>
-                    </div>
-
-                    <div class="span3" id="footerArea3">
+                    <div class="col-4" id="footerArea3">
                         <h3>Horaires de location</h3>
                         <p>Les locations peuvent s'effectuer tous les jours de la semaine en haute saison de 07h à 19h et en basse saison, les jours ouvrables de 8h à 18h.<br>
                             Vous pouvez aussi passer par le site. Pour le retrait et le dépot, vous devrez passer au guichet automatique à l'arrière du magasin</p>
                     </div>
 
-                    <div class="span3" id="footerArea4">
+                    <div class="col-4" id="footerArea4">
                         <h3>Contacts</h3>
 
                         <ul id="contact-info">
                             <li>
                                 <i class="general foundicon-phone icon"></i>
                                 <span class="field">Téléphone :</span>
-                                <br />
+                                <br/>
                                 +41 27 890 12 34
                             </li>
                             <li>
                                 <i class="general foundicon-mail icon"></i>
                                 <span class="field">Email :</span>
-                                <br />
+                                <br/>
                                 <a href="mailto:info@rentasnow.com" title="Email">info@rentasnow.com</a>
                             </li>
                             <li>
                                 <i class="general foundicon-home icon" style="margin-bottom:50px"></i>
                                 <span class="field">Addresse :</span>
-                                <br />
-                                12 Rue de la Glisse<br />
-                                2704 Sautons, Valais<br />
+                                <br/>
+                                12 Rue de la Glisse<br/>
+                                2704 Sautons, Valais<br/>
                                 Suisse
                             </li>
                         </ul>
                     </div>
 
                 </div>
-                <br /><br />
+                <br/><br/>
 
-                <div class="row-fluid">
-                    <div class="span12">
-                        <p class="copyright">Copyright © 2019 Rent A Snow. All Rights Reserved.</p>
-                        <p class="social_bookmarks">
-                            <a href="#"><i class="social foundicon-facebook"></i> Facebook</a>
-                            <a href=""><i class="social foundicon-twitter"></i> Twitter</a>
-                            <a href="#"><i class="social foundicon-pinterest"></i> Pinterest</a>
-                            <a href="#"><i class="social foundicon-rss"></i> Rss</a>
-                        </p>
+                <div class="row text-center">
+                    <div class="copyright">Copyright © 2019 Rent A Snow. All Rights Reserved.</div>
+                </div>
+                <div class="row text-center">
+                    <div class="social_bookmarks">
+                        <a href="#"><i class="social foundicon-facebook"></i> Facebook</a>
+                        <a href=""><i class="social foundicon-twitter"></i> Twitter</a>
+                        <a href="#"><i class="social foundicon-pinterest"></i> Pinterest</a>
+                        <a href="#"><i class="social foundicon-rss"></i> Rss</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <br /><br /><br />
-
-    <script src="node_modules/jquery/dist/jquery.js"></script>
-    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
-
-
-    <script src="assets/carousel/jquery.carouFredSel-6.2.0-packed.js" type="text/javascript"></script>
-    <script type="text/javascript">$('#list_photos').carouFredSel({ responsive: true, width: '100%', scroll: 2, items: {width: 320,visible: {min: 2, max: 6}} });</script>
-    <script src="assets/camera/scripts/camera.min.js" type="text/javascript"></script>
-    <script src="assets/easing/jquery.easing.1.3.js" type="text/javascript"></script>
-    <script type="text/javascript">function startCamera() {$('#camera_wrap').camera({ fx: 'scrollLeft', time: 2000, loader: 'none', playPause: false, navigation: true, height: '35%', pagination: true });}$(function(){startCamera()});</script>
+    <br/><br/><br/>
 
 </body>
 </html>
