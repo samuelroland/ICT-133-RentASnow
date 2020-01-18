6a1b481c71039e1da947d9277d456aeda6c5af55
samuelroland <samuel.roland@cpnv.ch>
2020-01-18 20:58:57 +0100

    controler createaccount() controle des données et gestion de wantnews value on ou off

--- controler/controler.php
+++ controler/controler.php
@@ -32,7 +32,6 @@ function trylogin($email, $password)
     if (isset($_SESSION['user']) == false) {
         $_SESSION['failed'] = true;
     }
-
     home(); //au lieu d'afficher la vue home on utilise la fonction home() pour avoir les news avec.
 }
 
@@ -48,10 +47,10 @@ function createaccount()
     $title = "Créer un compte chez RentASnow";
     $description = "3 minutes pour remplir ce formulaire. Veuillez rentrer les informations suivantes qui serviront à créer votre compte...";
 
-    if (isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['password']) && $_POST['password'] == $_POST['password2'] && isset($_POST['birthdate']) && $_POST['haslawsaccepted'] == true) {    //si tous les champs sont remplis et corrects alors on a recu les informations du formulaire envoyé et les informations sont valides.
+    //Si des champs sont remplis alors on a recu les informations du formulaire envoyé
+    if (isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['password']) && $_POST['password'] == $_POST['password2'] && isset($_POST['birthdate']) && $_POST['haslawsaccepted'] == "on") {    //si tous les champs ont une valeur et qu'ils sont cohérent alors les informations sont valides.
 
         //Vérifier qu'un utilisateur n'a pas la meme adresse mail.
-        $_SESSION['error'] = "";
         $listUsers = getUsers();
         foreach ($listUsers as $userinrun) {    //scanner toutes les personnes inscrites.
             if ($_POST['email'] == $userinrun['email']) {   //si un user actuel a déjà la meme adresse email:
@@ -59,17 +58,22 @@ function createaccount()
             }
         }
 
+        //Traitement des valeurs booléen avec case à cocher:
+        if ($_POST['wantnews'] == "on") {
+            $wantnews = true;
+        } else {
+            $wantnews = false;
+        }
         if ($_SESSION['error'] != "emailalreadytaken") {
             //On peut donc les stocker dans la liste des utilisateurs:
-            addUser($_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['password'], $_POST['birthdate'], $_POST['wantnews']);
-
+            addUser($_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['password'], $_POST['birthdate'], $wantnews);
             trylogin($_POST['email'], $_POST['password']);  //on peut directement connecter l'utilisateur
         } else {
-            require_once 'view/createaccount.php';  //on doit recommencer le formulaire si erreur de mail.
+            require_once 'view/createaccount.php'; //on doit recommencer le formulaire si erreur de mail.
         }
 
     } else {
-        require_once 'view/createaccount.php'; //sinon le formulaire n'a pas été rempli donc on affiche le formulaire
+        require_once 'view/createaccount.php';//sinon le formulaire n'a pas été rempli donc on affiche le formulaire
     }
 
 }
