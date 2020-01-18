1a2581d4c246e3751490166d42249ba80f76b3f0
samuelroland <samuel.roland@cpnv.ch>
2020-01-18 20:52:13 +0100

    createaccount update design and form action and email msg
    changed design inputs and added msg mail double and fix the form action through the querystring

--- view/createaccount.php
+++ view/createaccount.php
@@ -5,36 +5,43 @@
  *  Creation date: 09.01.2020
  */
 ob_start();
-if ($_SESSION['error'] == "emailalreadytaken"){ //si email déjà utilisé
+if ($_SESSION['error'] == "emailalreadytaken") { //si email déjà utilisé
     echo "<p class='alert-danger'>Cet email est déjà utilisé par un autre utilisateur...</p>";
 }
 ?>
-<form action="/?createaccount" method="POST" class="form-group">
-    <div>
-        <label for="firstname">Prénom</label>
-        <input type="text" id="firstname" name="firstname" class="form-group form-control" required>
+<form action="/?action=createaccount" method="POST" class="form-group">
+    <br>
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <label for="firstname">Prénom</label>
+            <input type="text" id="firstname" name="firstname" class="form-group form-control" required>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <label for="lastname">Nom de famille</label>
+            <input type="text" id="lastname" name="lastname" class="form-group form-control" required>
+        </div>
     </div>
-    <div>
-        <label for="lastname">Nom de famille</label>
-        <input type="text" id="lastname" name="lastname" class="form-group form-control" required>
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" class="form-group form-control" required>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <label for="birthdate">Date de naissance</label>
+            <input type="date" name="birthdate" id="birthdate" class="form-group form-control" required>
+        </div>
     </div>
-    <div>
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" class="form-group form-control" required>
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <label for="password">Mot de passe assez sécurisé...</label>
+            <input type="password" name="password" id="password" class="form-group form-control" required>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <label for="password2">Confirmation du mot de passe</label>
+            <input type="password" name="password2" id="password2" class="form-group form-control" required>
+        </div>
     </div>
-    <div>
-        <label for="birthdate">Date de naissance</label>
-        <input type="date" name="birthdate" id="birthdate" class="form-group form-control" required>
-    </div>
-    <div>
-        <label for="password">Mot de passe assez sécurisé...</label>
-        <input type="password" name="password" id="password" class="form-group form-control" required>
-    </div>
-    <div>
-        <label for="password2">Confirmation du mot de passe</label>
-        <input type="password" name="password2" id="password2" class="form-group form-control" required>
-    </div>
-
+    <br>
     <div class="row">
         <input type="checkbox" id="chk1" name="wantnews" class="col-1"><label class="col-11 infotocheck"
                                                                               for="wantnews">J'aimerai recevoir les
