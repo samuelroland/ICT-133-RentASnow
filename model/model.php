ed9b39b3e3fc370980164ac92c893c41ddd53e1c
samuelroland <samuel.roland@cpnv.ch>
2020-01-18 20:57:36 +0100

    model: addUser() finished

--- model/model.php
+++ model/model.php
@@ -15,8 +15,22 @@ function getUsers()
     return json_decode(file_get_contents("model/dataStorage/Users.json"), true);//recevoir la liste des utilisateurs
 }
 
-function addUser($firstname, $lastname, $email, $password, $birthdate, $wantnews)
+function addUser($firstname, $lastname, $email, $password, $birthdate, $wantnewsvalue)
 {
+
+    $listUsers = getUsers();
+    $newUser = [
+        'firstname' => $firstname,
+        'lastname' => $lastname,
+        'email' => $email,
+        'password' => $password,
+        'birthdate' => $birthdate,
+        'wantnews' => $wantnewsvalue,
+        'date-inscription' => date("Y-m-d", time()),
+        'employe' => false  //by default
+    ];
+    $listUsers[] = $newUser;
+    file_put_contents("model/dataStorage/Users.json", json_encode($listUsers));
 
 }
 
