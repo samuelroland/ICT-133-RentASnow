eb50984703028292e90c09907dddd3b46e516bfe
samuelroland <samuel.roland@cpnv.ch>
2020-01-18 20:53:59 +0100

    script checkform for create an account fixed
    problem with checkbox "haslawsaccepted" et his state

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 document.addEventListener("DOMContentLoaded", init)
 
 function init() {
-    firstname.addEventListener("keyup", checkform)
     password.addEventListener("keyup", checkform)
     password2.addEventListener("keyup", checkform)
     birthdate.addEventListener("keyup", checkform)
+    chk2.addEventListener("change", checkform)
 }
 
 function checkform() {
-    if (birthdate.value > "1999-01-01" && birthdate.value < "2020-01-01" && password.value === password2.value) {
+    if (birthdate.value > "1900-01-01" && birthdate.value < "2020-01-01" && document.getElementById('password').value === document.getElementById('password2').value && document.getElementById('chk2').checked == true) {
         btnsubmit.disabled = false
         return true
     } else {
         btnsubmit.disabled = true
-        alert(password.value)
-    return false
+        return false
     }
 }
